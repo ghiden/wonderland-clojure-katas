a4fa358eb66448ef6f73bb7b486c9dbdefc4de9a
Hidenari Nozaki <hidenari@gmail.com>
2015-12-05 10:00:20 +1300

    Fix wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn checkDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn check [wondernum]
+  (and (= 6 (count (str wondernum)))
+       (checkDigits? wondernum (* 2 wondernum))
+       (checkDigits? wondernum (* 3 wondernum))
+       (checkDigits? wondernum (* 4 wondernum))
+       (checkDigits? wondernum (* 5 wondernum))
+       (checkDigits? wondernum (* 6 wondernum))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (take 1 (filter check (range 100000 999999)))))
