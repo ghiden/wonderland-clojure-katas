fa19985f58150cda502b4119179483eee487c971
Hidenari <hidenari@gmail.com>
2016-06-29 21:34:32 +1200

    Add a reference to a better version

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -37,3 +37,33 @@
     (if (empty? result)
       result
       (concat result (list word2)))))
+
+;; a better version using tree-seq https://github.com/gensym/wonderland-clojure-katas/blob/doublets-solution/doublets/src/doublets/solver.clj
+
+;; (defn- one-away? [word1 word2]
+;;   (and (= (count word1) (count word2))
+;;        (= 1 (apply + (map
+;;                       #(if (= %1 %2) 0 1)
+;;                       word1 word2)))))
+
+;; (defn- start-expansion [word]
+;;   [word])
+
+;; (defn- expand [dict path]
+;;   (let [word (last path)
+;;         seen (into #{} path)]
+;;     (->> dict
+;;          (filter (complement seen))
+;;          (filter (partial one-away? word))
+;;          (map (fn [w] (conj path w))))))
+
+;; (defn- expansions [dict word]
+;;   (tree-seq (constantly true)
+;;             (partial expand dict)
+;;             (start-expansion word)))
+
+;; (defn doublets [word1 word2]
+;;   (let [matches (filter (fn [path] (= word2 (last path))) (expansions words word1))]
+;;     (if-let [shortest  (first (sort-by count matches))]
+;;       shortest
+;;             [])))
